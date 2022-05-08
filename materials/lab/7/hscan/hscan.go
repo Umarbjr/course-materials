318c809a8a31e19debdbf31c92ae2a40538b9d29
ubalogun <ubalogun@uwyo.edu>
2022-05-08 04:47:30 -0400

    Lab 7 finished

--- materials/lab/7/hscan/hscan.go
+++ materials/lab/7/hscan/hscan.go
@@ -8,14 +8,21 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
+	// "time"
 )
 
 //==========================================================================\\
-
+// var wg sync.WaitGroup
+var mutex = &sync.RWMutex{}
 var shalookup map[string]string
 var md5lookup map[string]string
 
-func GuessSingle(sourceHash string, filename string) {
+// func init() {
+// 	// shalookup := make()
+// }
+
+func GuessSingle(sourceHash string, filename string) string {
 
 	f, err := os.Open(filename)
 	if err != nil {
@@ -24,41 +31,96 @@ func GuessSingle(sourceHash string, filename string) {
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
-
+	var ret string = ""
 	for scanner.Scan() {
 		password := scanner.Text()
 
 		// TODO - From the length of the hash you should know which one of these to check ...
 		// add a check and logicial structure
-
-		hash := fmt.Sprintf("%x", md5.Sum([]byte(password)))
-		if hash == sourceHash {
-			fmt.Printf("[+] Password found (MD5): %s\n", password)
+		if len(sourceHash) == 32 {
+			hash := fmt.Sprintf("%x", md5.Sum([]byte(password)))
+			if hash == sourceHash {
+				fmt.Printf("[+] Password found (MD5): %s\n", password)
+				ret = password
+				return password
+			}
+		} else if len(sourceHash) == 64 {
+			hash := fmt.Sprintf("%x", sha256.Sum256([]byte(password)))
+			if hash == sourceHash {
+				fmt.Printf("[+] Password found (SHA-256): %s\n", password)
+				ret = password
+				return password
+			}
+		} else {
+			// fmt.Printf("[+] Password not found (MD5):")
 		}
 
-		hash = fmt.Sprintf("%x", sha256.Sum256([]byte(password)))
-		if hash == sourceHash {
-			fmt.Printf("[+] Password found (SHA-256): %s\n", password)
-		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatalln(err)
 	}
+	return ret
+}
+
+func GenHash(hashtype string, pass string) {
+	// shalookup = make(map[string]string)
+	// md5lookup = make(map[string]string)
+	// wg.Add(2)
+	// defer wg.Done()
+	// log.Println("pass: ", pass)
+	// time.Sleep(time.Second)
+	if hashtype == "md5" {
+		hash := fmt.Sprintf("%x", md5.Sum([]byte(pass)))
+		mutex.Lock()
+		md5lookup[hash] = pass
+		mutex.Unlock()
+
+		// log.Println("in here\n\n\n")
+	} else if hashtype == "sha256" {
+		// time.Sleep(time.Second)
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(pass)))
+		// mutex.Lock()
+		shalookup[hash] = pass
+		// mutex.Unlock()
+	}
+	// wg.Wait()
+	// log.Println("sha is: ", shalookup)
 }
 
 func GenHashMaps(filename string) {
 
+	shalookup = make(map[string]string)
+	md5lookup = make(map[string]string)
 	//TODO
 	//itterate through a file (look in the guessSingle function above)
 	//rather than check for equality add each hash:passwd entry to a map SHA and MD5 where the key = hash and the value = password
 	//TODO at the very least use go subroutines to generate the sha and md5 hashes at the same time
 	//OPTIONAL -- Can you use workers to make this even faster
 
+	// var wg sync.WaitGroup
+
+	// wg.Add(1)
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		password := scanner.Text()
+
+		GenHash("sha256", password)
+		go GenHash("md5", password)
+
+	}
+
 	//TODO create a test in hscan_test.go so that you can time the performance of your implementation
 	//Test and record the time it takes to scan to generate these Maps
 	// 1. With and without using go subroutines
 	// 2. Compute the time per password (hint the number of passwords for each file is listed on the site...)
+
 }
 
 func GetSHA(hash string) (string, error) {
@@ -68,12 +130,20 @@ func GetSHA(hash string) (string, error) {
 
 	} else {
 
-		return "", errors.New("password does not exist")
+		return "", errors.New("sha password does not exist")
 
 	}
 }
 
 //TODO
 func GetMD5(hash string) (string, error) {
-	return "", errors.New("not implemented")
+	password, ok := md5lookup[hash]
+	if ok {
+		return password, nil
+
+	} else {
+
+		return "", errors.New("md5 password does not exist")
+
+	}
 }
--- materials/lab/7/main/main.go
+++ materials/lab/7/main/main.go
@@ -2,27 +2,36 @@ package main
 
 import (
 	"hscan/hscan"
+	"log"
+	"os"
 )
 
 func main() {
 
+	if len(os.Args) != 2 {
+		log.Fatalln("Usage: main <password_file>")
+	}
 	//To test this with other password files youre going to have to hash
 	var md5hash = "77f62e3524cd583d698d51fa24fdff4f"
+	// log.Println("len of md: ", len(md5hash))
 	var sha256hash = "95a5e1547df73abdd4781b6c9e55f3377c15d08884b11738c2727dbd887d4ced"
+	// log.Println("len of sha: ", len(sha256hash))
 
 	//TODO - Try to find these (you may or may not based on your password lists)
 	var drmike1 = "90f2c9c53f66540e67349e0ab83d8cd0"
 	var drmike2 = "1c8bfe8f801d79745c4631d09fff36c82aa37fc4cce4fc946683d7b336b63032"
 
+	// not found in lulzsec.txt
+
 	// NON CODE - TODO
 	// Download and use bigger password file from: https://weakpass.com/wordlist/tiny  (want to push yourself try /small ; to easy? /big )
 
 	//TODO Grab the file to use from the command line instead; look at previous lab (e.g., #3 ) for examples of grabbing info from command line
-	var file = "wordlist.txt"
+	var file = os.Args[1]
 
 	hscan.GuessSingle(md5hash, file)
 	hscan.GuessSingle(sha256hash, file)
-	hscan.GenHashMaps(file)
-	hscan.GetSHA(sha256hash)
-	hscan.GetMD5(sha256hash)
+	hscan.GenHashMaps("lulzsec.txt")
+	hscan.GetSHA(drmike2)
+	hscan.GetMD5(drmike1)
 }
